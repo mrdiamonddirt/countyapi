6d9ea42dc7072aadbe6d4d38c372620179bcfc98
RowDog <mrdiamonddirt@gmail.com>
2022-11-05 12:48:16 +0000

    edit to api send

--- index.js
+++ index.js
@@ -1,45 +1,38 @@
-var fs = require('fs');
-   
-// json file with the data
-var data = fs.readFileSync('counties.json');
-   
-var counties = JSON.parse(data);
+var fs=require('fs');
+var data=fs.readFileSync('counties.json');
+var counties=JSON.parse(data);
 const express = require("express");
 const app = express();
-   
-// To solve the cors issue
 const cors=require('cors');
-    
-app.listen(process.env.PORT, 
-    () => console.log("Server Start at the Port"));
-    
+
+app.listen(process.env.PORT, () => console.log("Server Start at 5000 Port"));
+
 app.use(express.static('public'));
 app.use(cors());
-  
-// when get request is made, alldata() is called
-app.get('/counties', alldata);
-   
-function alldata(request, response) {
-       
-    // Returns all information about the County
+app.get('/counties',alldata);
+function alldata(request,response)
+{
     response.send(counties);
 }
-  
-app.get('/counties/:county/', searchcounties);
-  
-function searchcounties(request, response) {
-    var word = request.params.county;
-    word = word.charAt(0).toUpperCase()
-        + word.slice(1).toLowerCase();
-       
-    if(counties[word]) {
-        var reply = counties[word];         
-    }
-    else {
-        var reply = {
-            status:"Not Found"
-        }
-    }
-       
-    response.send(reply);
+app.get('/counties/:county/',searchElement);
+function searchElement(request,response)
+{
+	var word=request.params.county;
+	word=word.charAt(0).toUpperCase()+word.slice(1).toLowerCase();
+	console.log(word);
+	//console.log(elements[word]);
+	if(counties[word])
+	{
+		var reply=counties[word];
+		
+	}
+	else
+	{
+		var reply={
+			status:"Not Found"
+		}
+	}
+    console.log(reply.boil);
+	response.send(reply);
+
 }
